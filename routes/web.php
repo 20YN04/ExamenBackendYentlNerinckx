bd9507a42ee8c7d08a2af1cc2b7bc3d958c994cd
20YN04 <Yentl.nerinckx@icloud.com>
2026-03-18 10:52:18 +0100

    Refactor routes to use CourseController for course-related actions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Coursecontroller::class, 'index'])->name('courses.index');
+Route::get('/courses/create', function(){
+    return view('courses.create');
+
+})->name('courses.create');
+Route::post('/courses', [Coursecontroller::class, 'store'])->name('courses.store');
+Route::post('courses/{course}/toggle', [CourseController::class, 'toggle'])->name('courses.toggle');
+
